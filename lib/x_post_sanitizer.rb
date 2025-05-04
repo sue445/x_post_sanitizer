24c5083992137414d61d1d9c1793a1ba18ee5169
sue445 <sue445@sue445.net>
2025-05-04 16:45:32 +0900

    Add skeletons

--- lib/x_post_sanitizer.rb
+++ lib/x_post_sanitizer.rb
@@ -4,5 +4,48 @@ require_relative "x_post_sanitizer/version"
 
 module XPostSanitizer
   class Error < StandardError; end
-  # Your code goes here...
+
+  # Sanitize X Post (formerly Twitter Tweet)
+  #
+  # @param status [Hash] Response of `GET statuses/show/:id`
+  # @param use_retweeted_tweet [Boolean] Whether use original retweeted tweet if exists
+  # @param expand_url          [Boolean] Whether expand url in tweet (e.g. `t.co` url -> original url)
+  # @param remove_media_url    [Boolean] Whether remove media url in tweet
+  # @param unescape            [Boolean] Whether unescape in tweet (e.g. `(&gt; &lt;)` -> `(> <)`)
+  #
+  # @return [String] Sanitized text in status
+  #
+  # @see https://developer.x.com/en/docs/x-api/v1/tweets/post-and-engage/api-reference/get-statuses-show-id
+  def self.sanitize_text(status, use_retweeted_tweet: true, expand_url: true, remove_media_url: true, unescape: true)
+    # TODO: Do after
+  end
+
+  # @param status [Hash] Response of `GET statuses/show/:id`
+  # @param text [String]
+  #
+  # @return [String]
+  #
+  # @see https://developer.x.com/en/docs/x-api/v1/tweets/post-and-engage/api-reference/get-statuses-show-id
+  def self.expand_urls_text(status, text)
+    # TODO: Do after
+  end
+
+  # @param status [Hash] Response of `GET statuses/show/:id`
+  #
+  # @return [String] `full_text` attribute if exist
+  #
+  # @see https://developer.x.com/en/docs/x-api/v1/tweets/post-and-engage/api-reference/get-statuses-show-id
+  def self.status_full_text(status)
+    # TODO: Do after
+  end
+
+  # @param status [Hash] Response of `GET statuses/show/:id`
+  # @param text [String]
+  #
+  # @return [String]
+  #
+  # @see https://developer.x.com/en/docs/x-api/v1/tweets/post-and-engage/api-reference/get-statuses-show-id
+  def self.remove_media_urls_in_status(status, text)
+    # TODO: Do after
+  end
 end
